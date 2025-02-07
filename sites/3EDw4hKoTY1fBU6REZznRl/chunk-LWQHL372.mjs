40331fa7043944292b3518a75b2e2e690597e005
zkCryptoBiz <158302791+zkCryptoBiz@users.noreply.github.com>
2025-02-07 14:46:30 +0200

    Add files via upload

---
+++ sites/3EDw4hKoTY1fBU6REZznRl/chunk-LWQHL372.mjs
@@ -0,0 +1,2 @@
+function t(a,e){return{description:"TYGA is the official meme coin of the rap icon Tyga, created for the community.",favicon:"https://framerusercontent.com/assets/1qrgAZSYMUgN64oF1hfYP3oCcQ.png",robots:"max-image-preview:large",title:"TYGA OFFICIAL MEME"}}export{t as a};
+//# sourceMappingURL=chunk-LWQHL372.mjs.map
